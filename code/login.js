7df32f0b8f67b5c64ec34dd9d7a736cba39cac2c
vocoder <vocoder@netspace.in>
2024-11-08 08:16:00 -0500

    Refacotr and fix QuickCode login.

--- code/login.js
+++ code/login.js
@@ -19,14 +19,14 @@ if (accessToken) {
     const login_method = loginForm.login_method.value;
     const username = loginForm.username.value;
     const password = loginForm.password.value;
-    const quickcode = loginForm.quickcode.value;
-    authenticate(login_method, username, password, quickcode);
+    const quickcode_secret = loginForm.quickcode_secret.value;
+    authenticate(login_method, username, password, quickcode_secret);
   });
 }
 
-function authenticate(login_method, username, password, quickcode) {
-  const endpoint = "";
-  const formbody = "";
+function authenticate(login_method, username, password, quickcode_secret) {
+  var endpoint = "";
+  var formbody = "";
   if(login_method === "username") {
     endpoint = "AuthenticateByName";
     formbody = JSON.stringify({
@@ -36,7 +36,7 @@ function authenticate(login_method, username, password, quickcode) {
   } else if (login_method === "quickcode") {
     endpoint = "AuthenticateWithQuickConnect";
     formbody = JSON.stringify({
-        Secret: quickcode
+        Secret: quickcode_secret
     });
   }
   fetch(`${API_URL}Users/` + endpoint, {
@@ -67,18 +67,45 @@ function authenticate(login_method, username, password, quickcode) {
   });
 }
 
+function initializeQuickConnect() {
+    fetch(`${API_URL}QuickConnect/Initiate`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `MediaBrowser Client="${client}", Device="${device}", DeviceId="${device_id}", Version="${client_version}"`,
+      },
+      body: ""
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Could not get QuickConnect code');
+    })
+    .then(data => {
+      const quickcode = data.Code;
+      const secret = data.Secret;
+      document.getElementById('quickcode').value = quickcode;
+      document.getElementById('quickcode_secret').value = secret;
+    })
+    .catch(error => {
+      displayErrorMessage(error.message);
+    });
+}
+
 function changeAuthenticationMethod(login_method) {
     if(login_method === "username") {
       document.getElementById('auth-by-quickcode').style.display = "none";
       document.getElementById('username').required = true;
       document.getElementById('password').required = true;
-      document.getElementById('quickcode').required = false;
       document.getElementById('auth-by-credentials').style.display = "block";
     } else if (login_method === "quickcode") {
+      const quickconnect_active = parseInt(document.getElementById('quickcode_status').value);
+      if(!quickconnect_active) initializeQuickConnect();
+      document.getElementById('quickcode_status').value = 1;
       document.getElementById('auth-by-credentials').style.display = "none";
       document.getElementById('username').required = false;
       document.getElementById('password').required = false;
-      document.getElementById('quickcode').required = true;
       document.getElementById('auth-by-quickcode').style.display = "block";
     }
 }
